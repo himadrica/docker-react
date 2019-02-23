a64a2a71f301f51f14c34c82bd1f8092c656e37c
Himadri Talukder <himadricuet@gmail.com>
2019-02-23 01:22:08 -0500

    initial commit

---
+++ build/precache-manifest.08f7a142564bf6cea622266160629606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "478df7f87a73645b9684",
+    "url": "/static/js/main.478df7f8.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "478df7f87a73645b9684",
+    "url": "/static/css/main.5d3b3c06.chunk.css"
+  },
+  {
+    "revision": "deef3ad493555be32e1e3002f21cc732",
+    "url": "/index.html"
+  }
+];
